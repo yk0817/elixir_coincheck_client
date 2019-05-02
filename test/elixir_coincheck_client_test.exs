29b96eeefde6c90c8c4dc80f1567b4791e4f0f59
山本　憲 <em072029@gmail.com>
2019-05-02 18:44:19 +0900

    add test case

--- test/elixir_coincheck_client_test.exs
+++ test/elixir_coincheck_client_test.exs
@@ -1,8 +1,26 @@
 defmodule ElixirCoincheckClientTest do
   use ExUnit.Case
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
   doctest ElixirCoincheckClient
 
-  test "greets the world" do
-    assert ElixirCoincheckClient.hello() == :world
+  setup_all do
+    HTTPoison.start
+  end
+
+  test "get balance" do
+    use_cassette "coincheck_balance" do
+      response = ElixirCoincheckClient.balance
+      assert response.body =~ ~r/\"success\":true/
+      assert response.status_code == 200
+    end
+  end
+
+  test "read_bank_account" do
+    use_cassette "read_bank_account" do
+      response = ElixirCoincheckClient.read_bank_account
+      assert response.body =~ ~r/\"success\":true/
+      assert response.status_code == 200
+    end
   end
 end
