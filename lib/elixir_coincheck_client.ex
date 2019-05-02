9dce02b60aeb91ebccba85818f3db5bfe10cd8ee
山本　憲 <em072029@gmail.com>
2019-05-02 17:45:53 +0900

    add delegate pattern

--- lib/elixir_coincheck_client.ex
+++ lib/elixir_coincheck_client.ex
@@ -6,4 +6,27 @@ defmodule ElixirCoincheckClient do
 
   defdelegate trades, to: ElixirCoincheckClient.API.Trades
 
+  defdelegate exchange_orders_rate(params), to: ElixirCoincheckClient.API.ExchangeOrdersRate
+
+  defdelegate rate(params), to: ElixirCoincheckClient.API.Rate
+
+  defdelegate read_bank_account, to: ElixirCoincheckClient.API.ReadBankAccount
+
+  defdelegate transactions, to: ElixirCoincheckClient.API.ExchangeOrdersTransactions
+
+  defdelegate balance, to: ElixirCoincheckClient.API.Balance
+
+  defdelegate deposit_bitcoin_history, to: ElixirCoincheckClient.API.DepositBitcoinHistory
+
+  defdelegate accounts, to: ElixirCoincheckClient.API.Accounts
+
+  defdelegate send_bitcoin_history, to: ElixirCoincheckClient.API.SendBitcoinHistory
+
+  defdelegate withdraw_history, to: ElixirCoincheckClient.API.WithdrawHistory
+
+  defdelegate record_bank_account(params), to: ElixirCoincheckClient.API.RecordBankAccount
+
+  defdelegate withdraw_execute(params), to: ElixirCoincheckClient.API.WithdrawExecute
+
+  defdelegate exchange_orders(params), to: ElixirCoincheckClient.API.ExchangeOrders
 end
