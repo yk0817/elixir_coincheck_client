21d84e50c63a0f59450a08d376dbc6e483211978
山本　憲 <em072029@gmail.com>
2019-05-03 11:54:46 +0900

    update mix

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule ElixirCoincheckClient.MixProject do
       app: :elixir_coincheck_client,
       version: "0.1.0",
       elixir: "~> 1.8",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,7 +25,21 @@ defmodule ElixirCoincheckClient.MixProject do
     [
       {:exvcr, "~> 0.10", only: :test },
       {:httpoison, "~> 1.4" },
-      {:poison, "~> 3.1" }
+      {:poison, "~> 3.1" },
+      {:ex_doc, ">= 0.0.0"}
     ]
   end
+
+  defp description() do
+    """
+    Coincheck client library for elixir
+    """
+  end
+
+  defp package do
+    [ maintainers: ["yk0817"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/yk0817/elixir_coincheck_client"} ]
+  end
+
 end
